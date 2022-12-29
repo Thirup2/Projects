4e6872d44a6ec34bc9ce8c9a51c1e23113cada15
Thirup2 <246946332@qq.com>
2022-12-29 17:00:44 +0800

    贪吃蛇-更新

---
+++ 贪吃蛇/UI/UI_head.h
@@ -0,0 +1,13 @@
+/* UI类型头文件 */
+#ifndef _UI_HEAD_H_
+#define _UI_HEAD_H_
+
+/* 包含头文件 */
+#include "../Bit/Bit.h"
+#include "../Coordinate/Coordinate.h"
+#include <curses.h>
+
+/* UI类型定义 */
+
+
+#endif
